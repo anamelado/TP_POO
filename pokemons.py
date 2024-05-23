c236ed1b4604a3d50c243511cfb7c62cd2f9ede8
anamelado <anaclarameladomn@gmail.com>
2024-05-23 18:13:31 -0300

    abstracted class Pokemon

--- pokemons.py
+++ pokemons.py
@@ -1,11 +1,13 @@
 from index import Ataques
+from abc import ABC, abstractmethod
 
-class Pokemon:
+class Pokemon(ABC):
 
     def __init__(self, nome:str, vida:int, tipo:str) -> None:
         self.nome = nome
         self.vida = vida
         self.tipo = tipo
 
+    @abstractmethod
     def atacar(self, pokemon2:'Pokemon', ataque:'Ataques') -> None: 
-        print(f"{self.nome} usou {ataque.nome}")
+        pass
