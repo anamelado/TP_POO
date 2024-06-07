9621f13f8bf7170e228d3e1a8f601919e550aa66
João Victor Vial <joaovictorvial@gmail.com>
2024-06-07 11:29:50 -0300

    corringindo erro

--- index.py
+++ index.py
@@ -1,2 +1,17 @@
 from pokemon import Pokemon
+from status import Status
+from ataques import Ataques
+from typing import Type
+
+class Pokemon(Status, Ataques):
+    def subir_de_nivel(self):
+        self.nivel = int(self.nivel) + 1
+        self.status_up(self.nivel)
+    
+    def atacar(self, pokemon: Type[Pokemon]):
+        pokemon.vida -= self.forca
+    
+    def status_up(self, nivel):
+        self.vida += nivel*0,5
+        self.forca += nivel*0,5
 
