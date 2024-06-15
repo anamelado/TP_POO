2d8f49c5936fd961a2300fb422ea57e69d6f6479
luciahelenacapanema <luciahelenarsc@gmail.com>
2024-06-15 16:10:26 -0300

    Definindo o tipo de retorno dos métodos

--- status.py
+++ status.py
@@ -9,13 +9,13 @@ class Status(ABC):                                                          # De
         self._nivel = nivel                                                 # Atribui o nível do Pokémon (com o prefixo "_" para indicar que é protegido)
 
     @property                                                               # Define um método getter (propriedade) para acessar o nível do Pokémon
-    def nivel(self):
+    def nivel(self) -> str:
         return self._nivel                                                  # Retorna o nível do Pokémon
     
     @nivel.setter                                                           # Define um método setter para modificar o nível do Pokémon
-    def nivel(self, lvl):
+    def nivel(self, lvl) -> None:
         self._nivel = lvl                                                   # Define o novo nível do Pokémon
 
     @abstractmethod                                                         # Define um método abstrato que deve ser implementado nas subclasses
-    def subir_de_nivel(self):                                               # Método abstrato para aumentar o nível do Pokémon
+    def subir_de_nivel(self) -> None:                                               # Método abstrato para aumentar o nível do Pokémon
         pass                                                                # Corpo do método vazio, deve ser implementado nas subclasses
